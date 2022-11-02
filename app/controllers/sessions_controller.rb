770094456ebd388d9e03842417facdd56141fa7d
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-11-02 16:37:56 -0600

    add oauth to hit pry in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
   def create
-    redirect_to dashboard_path
+    user = BackendService.register_user(user_params)
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path(user)
+  end
+
+private
+
+  def auth_hash
+    auth_hash = request.env['omniauth.auth']
+  end
+
+  def user_params
+    {
+    google_id: auth_hash['uid'],
+    email: auth_hash['info']['email'],
+    token: auth_hash['credentials']['token']
+    }
   end
 end
