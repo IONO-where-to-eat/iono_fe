5342da31a2f1e4ecb7ff9eff6ee2ab0ae83d4ec9
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-11-04 20:10:23 -0700

    Add factories for restaurant and user
    Co-authored-by: Benjamin Randolph <neb417@users.noreply.github.com>
    Co-authored-by: Thomas Hayes <thayes87@users.noreply.github.com>
    Co-authored-by: Rebecka Hendricks <rebeckahendricks@users.noreply.github.com>

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+    factory :restaurant do
+      id { Faker::Omniauth.google[:uid] }
+      name { Faker::Restaurant.name }
+      image_url { Faker::Internet.url }
+      is_closed { Faker::Boolean.boolean }
+      url { Faker::Internet.url }
+      review_count { Faker::Number.number(digits: 3) }
+      categories { Faker::Restaurant.type }
+      rating {Faker::Number.between(from: 1, to: 5)}
+      transactions { ['takeout', 'delivery', 'dine-in'].sample }
+      price { ['$','$$','$$$','$$$$','$$$$$'].sample }
+      location { [Faker::Address.full_address] }
+      display_phone { Faker::PhoneNumber.cell_phone }
+      distance { Faker::Number.between(from: 100, to: 100000) }
+    end
+  end
