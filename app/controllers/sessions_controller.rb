3264179739948b85ebab64061422c01463bd6d8d
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-11-06 15:09:17 -0800

    Add logout button, bootstrap
    Co-authored-by: Rebecka Hendricks <rebeckahendricks@users.noreply.github.com>
    Co-authored-by: Benjamin Randolph <neb417@users.noreply.github.com>
    Co-authored-by: Thomas Hayes <thayes87@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,12 @@ class SessionsController < ApplicationController
     redirect_to dashboard_path(user_id)
   end
 
+  def logout
+    session.delete :user_id
+    session.delete :email
+    redirect_to root_path
+  end
+
 private
 
   def auth_hash
